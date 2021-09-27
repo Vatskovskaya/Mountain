e6f3aad2ee16ba1c199310ada30c295ef31616c8
Sasha <86354815+Vatskovskaya@users.noreply.github.com>
2021-09-27 18:20:11 +0300

    adaptive slider

--- js/main.js
+++ js/main.js
@@ -3,12 +3,21 @@ $(document).ready(function(){
     arrows: true,
     slidesToShow: 3,
     slidesToScroll: 3,
-      responsive:[{
-        breakpoint: 927,
-        settings: {
-          slidesToShow: 2,
-        slidesToScroll: 2,
+      responsive:[
+        {
+          breakpoint: 927,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
         }
-      }]
+      }
+    ]
     });
   });
