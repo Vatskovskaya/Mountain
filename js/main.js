9e03ec250a3961005ce651a3450f21c4bb9ae7dd
Sasha <86354815+Vatskovskaya@users.noreply.github.com>
2022-02-22 11:08:28 +0200

    add popup + search line
    practice jquery. make a login popup and search line

--- js/main.js
+++ js/main.js
@@ -1,26 +1,51 @@
 $(document).ready(function(){
+
   $('.header__burger').click(function(event) {
     $('.header__burger,.header__navigation__list').toggleClass('active');
-  })
-    $('.popular__box').slick({
+  });
+
+  $('.popular__box').slick({
     arrows: true,
     slidesToShow: 3,
     slidesToScroll: 3,
-      responsive:[
-        {
-          breakpoint: 927,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2,
-          }
-        },
+    responsive:[
+      {
+        breakpoint: 927,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        }
+      },
         {
           breakpoint: 600,
           settings: {
             slidesToShow: 1,
             slidesToScroll: 1,
+          }
         }
-      }
     ]
-    });
   });
+
+  $(document).on('click', '.header__search', function(event) {
+    event.preventDefault();
+    // if($('.search').css('display') == 'none') {
+    //   $('.search').slideDown(300)
+    // }else{
+    //   $('.search').slideUp(300)
+    // }
+    $('.search').toggleClass('active'); // отображения панели инпута поиск через добавление класса
+  });
+  $(document).on('click', '.header__login', function (event) {
+    event.preventDefault();
+    $('.popup-wrapper').toggleClass('active');
+  
+    $('.wrapper').css('filter','blur(5px)');
+
+  });
+
+  $(document).on('click', '.close_popup', function (event) {
+    event.preventDefault();
+    $('.popup-wrapper').removeClass('active');
+    $('.wrapper').css('filter','none');
+  });
+});
